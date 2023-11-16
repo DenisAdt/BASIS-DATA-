3614636bbceb9dc8104b5d9d6396820c7313a03f
DenisAdt <145324896+DenisAdt@users.noreply.github.com>
2023-11-16 22:37:43 +0700

    Create Jawaban2.sql

---
+++ UTS/Jawaban2.sql
@@ -0,0 +1,11 @@
+insert into mahasiswa (id, first_name, last_name, nim_mahasiswa, jurusan, adress) VALUES
+( 1 ,'denis','aditiansyah','20220898765','teknik informatika','citra'),
+( 2 , 'adit','tian','20220801086','teknik informatika','citra');
+
+insert into dosen (id, kode_dosen, first_name, last_name, fakultas_id) VALUES
+( 1 ,'23','jerfri','sanupurwa','23'),
+( 2	,'24','bruce','lee','24');
+
+INSERT into mata_kuliah (id, nama_matkul, ruang_matkul) VALUES
+( 1 , 'basis data','CR304'),
+( 2 , 'wingchun','CR304');
